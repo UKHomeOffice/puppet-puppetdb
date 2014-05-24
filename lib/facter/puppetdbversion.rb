5c14838d0dc4cbda806c5fb77c0e5622174a870e
Chinedu Uzoka <acuzoka@gmail.com>
2014-05-24 23:01:18 +0100

    Use fact osfamily - makes things somewhat simpler

--- lib/facter/puppetdbversion.rb
+++ lib/facter/puppetdbversion.rb
@@ -6,24 +6,25 @@ Facter.add("puppetdbversion") do
   setcode do
     versionfile = '/var/lib/puppet/puppetdbversion'
     val = ''
-    if File.exists?(versionfile) and (File.stat(versionfile).mtime.to_i + (12*60*60) >= Time.now().to_i)
+    if File.exists?(versionfile) and ((File.stat(versionfile).mtime.to_i + (12*60*60) >= Time.now().to_i) and File.size(versionfile) > 0)
       %x{cat #{versionfile}}.chomp.to_f
     else
       output = %x{/usr/bin/java -jar /usr/share/puppetdb/puppetdb.jar version 2>/dev/null | grep '^version'}
       output.chomp!
       if output.length >0
         val = output.split('=')[-1]
-        val.to_f
-      elsif Facter.value(:operatingsystem) == /RedHat|Fedora/
+      elsif Facter.value(:osfamily) == 'RedHat'
         output = %x{rpm -q puppetdb --qf '%{VERSION}' 2>/dev/null}.to_f
         unless output.nil?
           val = output
-          val.to_f
         end
       else
         ''
       end
+      unless val.nil?
+        File.open(versionfile,'w') {|x| x.write(val)}
+        val.to_f
+      end
     end
-    File.open(versionfile,'w') {|x| x.write(val)} unless val.nil?
   end
 end
