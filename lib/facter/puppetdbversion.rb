1d8985184b7f55ab8e06879d1a1c8459da073790
Chinedu Uzoka <acuzoka@gmail.com>
2014-05-28 23:09:04 +0100

    Reworked some bits here

--- lib/facter/puppetdbversion.rb
+++ lib/facter/puppetdbversion.rb
@@ -6,24 +6,27 @@ Facter.add("puppetdbversion") do
   setcode do
     versionfile = '/var/lib/puppet/puppetdbversion'
     val = ''
-    if File.exists?(versionfile) and ((File.stat(versionfile).mtime.to_i + (12*60*60) >= Time.now().to_i) and File.size(versionfile) > 0)
+    if File.exists?(versionfile) and ((File.stat(versionfile).mtime.to_i + (12*60*60) >= Time.now().to_i) or File.size(versionfile) > 0)
       %x{cat #{versionfile}}.chomp.to_f
     else
-      output = %x{/usr/bin/java -jar /usr/share/puppetdb/puppetdb.jar version 2>/dev/null | grep '^version'}
+      output = Facter::Util::Resolution.execr("/usr/bin/java -jar /usr/share/puppetdb/puppetdb.jar version 2>/dev/null | grep '^version'")
       output.chomp!
       if output.length >0
         val = output.split('=')[-1]
       elsif Facter.value(:osfamily) == 'RedHat'
-        output = %x{rpm -q puppetdb --qf '%{VERSION}' 2>/dev/null}.to_f
-        unless output.nil?
-          val = output
+        output = Facter::Util::Resolution.exec("/bin/rpm -q puppetdb --qf '%{VERSION}' 2>/dev/null")
+        if output
+          match = /^[\d.]+\d$/.match(output)
+          if match 
+            val = output.to_f
+          end
         end
       else
-        ''
+        nil
       end
       unless val.nil?
         File.open(versionfile,'w') {|x| x.write(val)}
-        val.to_f
+        val
       end
     end
   end
