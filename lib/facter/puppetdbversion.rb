f894cd1dfc90453aa3803b6ec6dc80da28e23a72
Chinedu Uzoka <acuzoka@gmail.com>
2014-07-21 17:02:02 +0100

    Fixed error display on non puppetdb hosts

--- lib/facter/puppetdbversion.rb
+++ lib/facter/puppetdbversion.rb
@@ -1,23 +1,25 @@
 # puppetdbversion.rb
 # Supply the PuppetDB version as a fact
-# 
+#
 Facter.add("puppetdbversion") do
-  confine :kernel => %w{Linux FreeBSD OpenBSD SunOS HP-UX GNU/kFreeBSD}
+  confine :kernel => 'Linux'
+  confine :osfamily => 'RedHat'
   setcode do
     versionfile = '/var/lib/puppet/puppetdbversion'
     val = ''
+    output = ''
     if File.exists?(versionfile) and ((File.stat(versionfile).mtime.to_i + (12*60*60) >= Time.now().to_i) or File.size(versionfile) > 0)
       %x{cat #{versionfile}}.chomp.to_f
     else
       output = Facter::Util::Resolution.exec("/usr/bin/java -jar /usr/share/puppetdb/puppetdb.jar version 2>/dev/null | grep '^version'")
-      output.chomp!
-      if output.length >0
+      if ! output.nil? and output.length >0
+        output.chomp!
         val = output.split('=')[-1]
       elsif Facter.value(:osfamily) == 'RedHat'
         output = Facter::Util::Resolution.exec("/bin/rpm -q puppetdb --qf '%{VERSION}' 2>/dev/null")
         if output
           match = /^[\d.]+\d$/.match(output)
-          if match 
+          if match
             val = output.to_f
           end
         end
